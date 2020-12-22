7355be2cd00481111f4661a4b8e6095b120e77e2
Ankit Kalauni <70426091+Ankitkalauni@users.noreply.github.com>
2020-12-22 21:17:05 +0530

    Add files via upload
    Added logistic regression python file, dataset and confusion matrix svg file

---
+++ Classification/Logistic-Regression/logistic-Regression.py
@@ -0,0 +1,126 @@
+#Logistic Regression with Python
+
+#libs
+import pandas as pd
+import pylab as pl
+import numpy as np
+import scipy.optimize as opt
+from sklearn import preprocessing
+%matplotlib inline 
+import matplotlib.pyplot as plt
+
+
+!wget -O ChurnData.csv https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-ML0101EN-SkillsNetwork/labs/Module%203/data/ChurnData.csv
+
+df = pd.read_csv('ChurnData.csv')
+churn_df = df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip',   'callcard', 'wireless','churn']]
+churn_df['churn'] = churn_df['churn'].astype('int')
+
+#practice code
+print("Columns: ", churn_df.columns)
+print("\nShape of DataFrame: ", churn_df.shape)
+
+from sklearn.model_selection import train_test_split
+X = churn_df[['tenure', 'age', 'address', 'income', 'ed', 'employ', 'equip']].values
+y = churn_df.iloc[:, -1]
+
+#normalization
+from sklearn.preprocessing import StandardScaler
+scalar = StandardScaler()
+X = scalar.fit_transform(X)
+
+
+#data spliting
+X_train, X_test, y_train,y_test = train_test_split(X, y, test_size = 0.2, random_state = 4)
+
+#Modeling (Logistic Regression with Scikit-learn)
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+LR = LogisticRegression(C=0.01, solver='liblinear').fit(X_train,y_train)
+
+#prediction
+yhat = LR.predict(X_test)
+yhat
+
+y_test = y_test.values.reshape(-1,1)
+yhat = yhat.reshape(-1,1)
+
+result = np.concatenate((y_test, yhat), axis = 1)
+
+yhat_prob = LR.predict_proba(X_test)
+yhat_prob
+
+#Evaluation
+from sklearn.metrics import jaccard_similarity_score
+jaccard_similarity_score(y_test, yhat)
+
+'''
+confusion matrix
+Another way of looking at accuracy of classifier is to look at confusion matrix.
+'''
+
+from sklearn.metrics import classification_report, confusion_matrix
+import itertools
+def plot_confusion_matrix(cm, classes,
+                          normalize=False,
+                          title='Confusion matrix',
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+    if normalize:
+        cm = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+        print("Normalized confusion matrix")
+    else:
+        print('Confusion matrix, without normalization')
+
+    print(cm)
+
+    plt.imshow(cm, interpolation='nearest', cmap=cmap)
+    plt.title(title)
+    plt.colorbar()
+    tick_marks = np.arange(len(classes))
+    plt.xticks(tick_marks, classes, rotation=45)
+    plt.yticks(tick_marks, classes)
+
+    fmt = '.2f' if normalize else 'd'
+    thresh = cm.max() / 2.
+    for i, j in itertools.product(range(cm.shape[0]), range(cm.shape[1])):
+        plt.text(j, i, format(cm[i, j], fmt),
+                 horizontalalignment="center",
+                 color="white" if cm[i, j] > thresh else "black")
+
+    plt.tight_layout()
+    plt.ylabel('True label')
+    plt.xlabel('Predicted label')
+    plt.savefig('confusion_matrix.png')
+print(confusion_matrix(y_test, yhat, labels=[1,0]))
+
+# Compute confusion matrix
+cnf_matrix = confusion_matrix(y_test, yhat, labels=[1,0])
+np.set_printoptions(precision=2)
+
+
+# Plot non-normalized confusion matrix
+plt.figure()
+plot_confusion_matrix(cnf_matrix, classes=['churn=1','churn=0'],normalize= False, \
+                      title='Confusion matrix')
+
+print (classification_report(y_test, yhat))
+
+
+from sklearn.metrics import log_loss
+log_loss(y_test, yhat_prob)
+
+
+# -----------------------------------------
+#practice code
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import confusion_matrix
+LR = LogisticRegression(C=0.02, solver='newton-cg').fit(X_train,y_train)
+yhat = LR.predict(X_test)
+yhat_prob = LR.predict_proba(X_test)
+from sklearn.metrics import jaccard_similarity_score
+jaccard_similarity_score(y_test, yhat)
+
